f9f70c80e82751bd146b2ab9863d30c8cdb3394f
Shankar30 <75795864+Shankar30@users.noreply.github.com>
2021-01-09 21:57:56 +0530

    Add file for Kotlin variables assignment

---
+++ song.kt
@@ -0,0 +1,24 @@
+val title: String = "Dope Track"
+val length: Double = 4.11
+val floatLength: Float = 4.11f
+val artist: String = "Yuvan Shankar Raja"
+val releaseTitle: String = "Pyaar Prema Kaadhal"
+val releaseGroupType: String = "Album + Soundtrack"
+val country: String = "India"
+val releaseYear: Int = 2018
+val releaseMonth: Int = 7
+val releaseDay: Int = 29
+val label: String = "U1 Records"
+
+print(title)
+print(length)
+print(floatLength)
+print(artist)
+print(releaseTitle)
+print(releaseGroupType)
+print(country)
+print(releaseYear)
+print(releaseMonth)
+print(releaseDay)
+print(label)
+
